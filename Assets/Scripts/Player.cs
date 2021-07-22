485aaf85ec2278cfecbaeccd957951284dc36396
znshak <ggm.s200029@ggh.goe.go.kr>
2021-07-22 21:16:22 +0900

    적 ai 동료 ai 70%

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    bool isGround = true;
-    bool isJump = true;
+    protected bool isGround = true;
 
-    Rigidbody2D rb;
-    StageManager stageManager;
+    protected Rigidbody2D rb;
+    protected StageManager stageManager;
+
+    public GameObject Monster;
 
     [Header("플레이어 스텟")]
     public float hp;
@@ -19,23 +20,24 @@ public class Player : MonoBehaviour
     public float jumpSpeed = 3f;
     public float attackRange;
 
-    void Start()
+    public float distance;
+
+    public void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         stageManager = FindObjectOfType<StageManager>();
         isGround = true;
     }
 
-    void Update()
+    public void Update()
     {
         PlayerMove();
         CheckGround();
 
         Debug.Log(isGround);
-
     }
 
-    void PlayerMove()
+    public void PlayerMove()
     {
         float h = Input.GetAxisRaw("Horizontal");
         
@@ -44,13 +46,21 @@ public class Player : MonoBehaviour
         if(Input.GetButtonDown("Jump") && isGround)
         {
             rb.AddForce(Vector2.up*jumpSpeed, ForceMode2D.Impulse);
-            isJump = true;
         }
     }
 
-    void CheckGround()
+    public virtual void DoppelgangerMove()
     {
-        isGround = Physics2D.OverlapCircle((Vector2)transform.position + new Vector2(0, -0.4f), 0.3f, 1 << LayerMask.NameToLayer("Ground"));
+        Vector2 dir = Monster.transform.position - transform.position;
+        distance = dir.sqrMagnitude;
+        float realDir = dir.x > 0 ? 1 : -1;
+
+        transform.Translate(new Vector2(realDir * speed * Time.deltaTime,0));
+    }
+
+    public void CheckGround()
+    {
+        isGround = Physics2D.OverlapCircle((Vector2)transform.position + new Vector2(0, -0.8f), 0.3f, 1 << LayerMask.NameToLayer("Ground"));
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
@@ -61,5 +71,18 @@ public class Player : MonoBehaviour
             Debug.Log("포탈포탈");
             stageManager.NextStage();
         }
+        if(other.transform.CompareTag("Ground"))
+        {
+            GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        if(other.transform.CompareTag("Ground"))
+        {
+            GetComponent<BoxCollider2D>().isTrigger = false;
+        }
+        
     }
 }

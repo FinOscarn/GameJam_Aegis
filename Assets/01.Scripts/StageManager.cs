aad78c6c14e7945d62c73ce0a9b66b2f90185160
znshak <ggm.s200029@ggh.goe.go.kr>
2021-07-23 17:58:14 +0900

    맵 구성

--- Assets/01.Scripts/StageManager.cs
+++ Assets/01.Scripts/StageManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class StageManager : MonoBehaviour
@@ -12,6 +13,8 @@ public class StageManager : MonoBehaviour
         public List<Transform> StartPosition = new List<Transform>();
     }
 
+    CinemachineConfiner cine;
+
     public StartPositionArray[] startPositionArrays;
 
 
@@ -22,6 +25,12 @@ public class StageManager : MonoBehaviour
     int curStage;
     int lastStage;
 
+    public Collider2D[] bgColider;
+    private void Start() 
+    {
+        cine = FindObjectOfType<CinemachineConfiner>();    
+    }
+
     public void NextStage()
     {
         curStage++;
@@ -29,7 +38,11 @@ public class StageManager : MonoBehaviour
         int mapIndex = curStage / 10;
         int randomIndex = Random.Range(0, startPositionArrays[mapIndex].StartPosition.Count);
 
+        cine.m_BoundingShape2D = bgColider[randomIndex];
+
         Player.transform.position = startPositionArrays[mapIndex].StartPosition[randomIndex].position;
         startPositionArrays[mapIndex].StartPosition.RemoveAt(randomIndex);
     }
+
+
 }
